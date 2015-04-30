c2dc7bf9fe46cd6e0fa849a0f939fb850196f980
Kevin R. McCraney <kevinrmccraney@gmail.com>
2015-04-30 14:31:01 -0400

    develop more robust functions

--- todo.sh
+++ todo.sh
@@ -2,11 +2,7 @@
 
 # this first part is carving up the script into parts
 # functionality should be todo -flag TASKNAME "meta"
-# if no taskname is provided, then syntax should be
-# todo help provides help 
-# todo init enables tracking
-# todo list lists all todos 
-# todo purge
+# if no taskname is provided, then syntax should be 
 
 if [ $1 ]
 then
@@ -28,30 +24,39 @@ echo "		todo purge	- deletes all todos;
 }
 
 function todo() {
-# git add .
-# git commit -m "add $1"
-# echo '$2' > $1.todo
-# the second argument should be optional, so:
+if [ $2 ]
+    echo "$2" > $1.todo
+else
+    touch $1.todo
+fi
+git add .
+git commit -m "add $1"
 }
 
 function todo_history() {
-# git log
+git log
 }
 
 function todo_init() {
+
 # check for presence of gitignore file that ignores everything but .todo
-# if gitignore is not there,
-# touch .gitignore containing
-# *
-# !*.todo
-# else perform a commit
-# 
+# grep .gitignore
+if
+    # gitignore is not there
+    echo "* /n !*.todo" > .gitignore
+else
+    echo "You've already performed this command. You probably want to perform a commit instead."
+fi
 }
 
 function todo_del() {
-# rm [ $1 ].todo
+rm $1.todo
+git add .
+git commit -m "delete $1"
 }
 
 function todo_purge() {
-# rm *.todo
+rm *.todo
+git add .
+git commit -m "delete all todos"
 }
