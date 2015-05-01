36dc86bf2592a550ef3f6e7210a340c38fd527a3
Kevin R. McCraney <kevinrmccraney@gmail.com>
2015-05-01 13:38:40 -0400

    tweak tracking

--- todo.sh
+++ todo.sh
@@ -2,15 +2,17 @@
 
 function help () {
 echo "todo - a script that uses obsessive-compulsion and git to get things done."
-echo "  be sure to isolate this from any other git-based projects."
-echo "  oh yeah; you have to get git installed for this to work. git it!"
+echo "		      be sure to isolate this from any other git-based projects."
+echo "			 you have to get git installed for this to work. git it!"
 echo ""
-echo "	syntax:	todo FILENAME 'optional description'"
+echo "		syntax:		  todo FILENAME 'optional description'"
 echo ""	
 echo "		todo init	- starts tracking todos in current directory";
 echo "		todo history	- lists all todos since last interaction with program";
 echo "		todo del FILE	- deletes FILE";
 echo "		todo purge	- deletes all todos";
+echo ""
+echo "								  Any questions?"
 }
 
 if [ $1 ]
@@ -29,20 +31,33 @@ then
     read -p  "This will purge all .todo files. Are you sure you want to? Press Y to confirm and any other key to cancel." RESP
     if [ "$RESP" = "y" ]
     then
-      rm *.todo
-      git add .
-      git commit -m 'delete all todos'
+      if [ -a .gitignore ]
+      then
+        rm *.todo
+        git add .
+        git commit -m 'delete all todos'
+      else
+        rm *.todo
+      fi
     else
       echo "You cancelled the .todo purge."
     fi
   ;;
   "history")
-    git log
+    if [ -a .gitignore ]
+    then
+      git log
+    else
+      echo "Sorry, you don't have tracking enabled."
+    fi
   ;;
   del)
     if [ $2 ]
     then
       rm $2.todo
+    fi
+    if [ -a .gitignore ]
+    then
       git add .
       git commit -m 'delete $2'
     fi
@@ -50,12 +65,15 @@ then
   *)
     if [ $2 ]
     then
-      echo '$2' > $1.todo
+      echo "$2" > $1.todo
     else
       touch $1.todo
     fi
-    git add .
-    git commit -m 'add $1'
+    if [ -a .gitignore ]
+    then
+      git add .
+      git commit -m 'add $1'
+    fi
   ;;
   esac
 else
