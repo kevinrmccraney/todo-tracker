871d95d515bbe7ffa9be3483a2f35d0f6e922435
Kevin R. McCraney <kevinrmccraney@gmail.com>
2015-05-01 12:31:50 -0400

    rework script

--- todo.sh
+++ todo.sh
@@ -1,62 +1,64 @@
 #!/bin/sh
 
-# this first part is carving up the script into parts
-# functionality should be todo -flag TASKNAME "meta"
-# if no taskname is provided, then syntax should be 
+function help () {
+echo "todo - a script that uses obsessive-compulsion and git to get things done."
+echo ""
+echo "	syntax:	todo FILENAME 'optional description'"
+echo "	
+echo "		todo init	- starts tracking todos in current directory (optional)";
+echo "		todo history	- lists all todos since last interaction with program";
+echo "		todo del FILE	- deletes FILE";
+echo "		todo purge	- deletes all todos";
+}
 
 if [ $1 ]
 then
-    # do some stuff
+  case "$1" in
+  init)
+  file= git
+    if [ -f "$file" ]
+    then
+      if [ grep -l '!*.todo'  *.gitignore ]
+        echo "Initalizing project tracking."
+        echo "* /n !*.todo" > .gitignore
+      else
+        echo "You've already started tracking. You probably want to perform a commit instead."
+      fi
+    else
+      echo "Sorry. You don't have all the dependencies. Download git and everything should work."
+    fi
+  purge)
+    read -p  "This will purge all .todo files. Are you sure you want to? Press Y to confirm and any other key to cancel." RESP
+    if [ "$RESP" = "y" ]; then
+      rm *.todo
+      git add .
+      git commit -m "delete all todos"
+    else
+      echo "You cancelled the .todo purge."
+    fi
+  ;;
+  history)
+    git log
+  ;;
+  del)
+    if [ $2 ]
+    then
+      rm $2.todo
+      git add .
+      git commit -m "delete $2"
+    fi
+  ;;
+  *)
+    if [ $2 ]
+      echo "$2" > $1.todo
+    else
+      touch $1.todo
+    fi
+    git add .
+    git commit -m "add $1"
+  ;;
+  esac
 else
-    help
+  help;
 fi
-
-
-function help() {
-echo "todo - a script that uses obsessive-compulsion and git to get things done."
-echo "
-echo "	syntax:	todo FILENAME "optional description"
-echo "	
-echo "		todo init	- starts tracking todos in current directory (optional);
-echo "		todo history	- lists all todos since last interaction with program;
-echo "		todo del FILE	- deletes FILE;
-echo "		todo purge	- deletes all todos;
-}
-
-function todo() {
-if [ $2 ]
-    echo "$2" > $1.todo
-else
-    touch $1.todo
-fi
-git add .
-git commit -m "add $1"
-}
-
-function history() {
-git log
-}
-
-function init() {
-
-# check for presence of gitignore file that ignores everything but .todo
-# grep -l '!*.todo'  *.gitignore
-if
-    # gitignore is not there
-    echo "* /n !*.todo" > .gitignore
-else
-    echo "You've already performed this command. You probably want to perform a commit instead."
-fi
-}
-
-function del() {
-rm [ $1 ].todo
-git add .
-git commit -m "delete $1"
-}
-
-function purge() {
-rm *.todo
-git add .
-git commit -m "delete all todos"
 }
